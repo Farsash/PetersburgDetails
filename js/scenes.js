cadf2b2343ebca7990c8f1408c810221e57863ea
Farsash <farsash2007@gmail.com>
2016-08-05 23:02:53 +0300

    office_material

--- js/scenes.js
+++ js/scenes.js
@@ -103,13 +103,58 @@
 				if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 				
 				var number_model = 0;
+				
+				var objects = [];
+			
+				var t_loader = new THREE.TextureLoader();
+				var stone_mat = new THREE.MeshBasicMaterial( { map: t_loader .load('models/stone_dif.jpg')} );
+				
+				var fun_mat = new THREE.MeshBasicMaterial( { map: t_loader .load('models/o/fun.jpg')} );
+				var ogr_mat = new THREE.MeshPhongMaterial( { color: 0x8f8f8f, specular: 0x282725, side:THREE.DoubleSide} );
+
 
 				var container, loader;
 				var camera, controls, scene, renderer;
 				var scene = new THREE.Scene();
 				var areaW = $('#container').width(); var areaH = $('#container').height();
+				
+				
+							
+				var stone = [{
+					path: 'models/o/fun.js',
+					mat: fun_mat,
+					name: 'stone',
+					scale: 1,
+					pos_x: 0,
+					pos_y: 0,
+					pos_z: 0,
+				},{
+					path: 'models/o/og1.js',
+					mat: ogr_mat,
+					name: 'stone',
+					scale: 1,
+					pos_x: 0,
+					pos_y: 0,
+					pos_z: 0,
+				}];	
+				
+				objects.push(stone)
+				
+				var stone2 = [{
+					path: 'models/stone.json',
+					mat: stone_mat,
+					name: 'stone',
+					scale: 0.1,
+					pos_x: 0,
+					pos_y: -3,
+					pos_z: 0,
+				}];	
+				
+				objects.push(stone2)
+					
 
 				init();
+				obj_add(objects[0]);
 				render();
 				animate();
 				
@@ -120,20 +165,21 @@
 				
 				function init() {
 					camera = new THREE.PerspectiveCamera( 60, areaW / areaH, 1, 80000 );
-					camera.position.y = 10;
-					camera.position.x = 10;
+					camera.position.y = 100;
+					camera.position.x = 100;
 					camera.lookAt(scene);
 					controls = new THREE.OrbitControls( camera );
 					controls.damping = 0.02;
 					controls.addEventListener( 'change', render );
-				
-
 					
-					var geometry = new THREE.PlaneGeometry( 20, 20, 10, 10 );
-					var material = new THREE.MeshBasicMaterial( {color: 0x00ffff, side: THREE.DoubleSide, wireframe: true, transparent: true} );
-					var plane = new THREE.Mesh( geometry, material );
-					plane.rotation.x = 90*(Math.PI/180)
-					scene.add( plane );
+					var light = new THREE.PointLight( 0xffffff, 1 );
+					light.position.set( 0, 108.53 , 56.36  );
+					scene.add( light );
+					
+					var light = new THREE.PointLight( 0xffffff, 0.8 );
+					light.position.set( 0, -108.53 , -56.36  );
+					scene.add( light );
+					
 				
 					renderer = new THREE.WebGLRenderer( { antialias: true } );
 					renderer.setClearColor( 0xebebe8 );
@@ -156,6 +202,25 @@
 				function render() {	
 					renderer.render( scene, camera );
 				}
+				
+				function obj_add(ht) {
+					loader = new THREE.JSONLoader();	
+					ht.forEach(function(o) {
+						loader.load(o.path,
+							function ( geometry, materials ) {
+								var object = new THREE.Mesh( geometry, o.mat );
+								object.scale.set(o.scale,o.scale,o.scale);
+								object.position.set(o.pos_x,o.pos_y,o.pos_z);
+								geometry.computeTangents();
+								object.name = o.name;
+								//remove_list.push(object.name);
+								scene.add(object);
+								render();
+							}
+						);
+					});
+				}
+	
 				
 							
 				$('#left_bt').click(function(e) {
